1a3a62bf2de2a7517e67073071f36c5951eb22fe
VandanaDamodara <damvandana@gmail.com>
2021-10-04 22:27:45 +0000

    twitterhelper

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/dao/helper/TwitterHttpHelper.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/dao/helper/TwitterHttpHelper.java
@@ -1,2 +1,28 @@
-package ca.jrvs.apps.twitter.dao.helper;public class TwitterHttpHelper {
+package ca.jrvs.apps.twitter.dao.helper;
+
+import java.net.URI;
+import oauth.signpost.OAuthConsumer;
+import oauth.signpost.commonshttp.CommonsHttpOAuthConsumer;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+public class TwitterHttpHelper implements HttpHelper{
+  private final OAuthConsumer consumer;
+  private final HttpClient httpClient;
+
+  public TwitterHttpHelper(String consumerKey, String consumerSecret, String accessToken, String tokenSecret){
+    consumer = new CommonsHttpOAuthConsumer(consumerKey, consumerSecret);
+    consumer.setTokenWithSecret(accessToken, tokenSecret);
+    httpClient = new DefaultHttpClient();
+  }
+  @Override
+  public HttpResponse httpPost(URI uri) {
+    return null;
+  }
+
+  @Override
+  public HttpResponse httpGet(URI uri) {
+    return null;
+  }
 }
