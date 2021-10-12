12b82104f5a3850e821f9477080ca91f1dbb0cfb
VandanaDamodara <damvandana@gmail.com>
2021-10-12 14:53:09 +0000

    updated

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/util/TweetBuilder.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/util/TweetBuilder.java
@@ -25,20 +25,3 @@ public class TweetBuilder {
 
   }
 }
-/**
-public class TweetBuilder {
-
-  public static Tweet buildTweet(String tweeterMessage, Double latitude, Double longitude) {
-    Tweet tempTweet = new Tweet();
-    Double[] coordinates = {latitude, longitude};
-    Coordinates location = new Coordinates();
-    String type = "Degrees.Minutes";
-    location.setcoordinates(coordinates);
-    location.setType(type);
-    tempTweet.setText(tweeterMessage);
-    tempTweet.setCoordinates(location);
-    return tempTweet;
-
-  }
-}
- **/
